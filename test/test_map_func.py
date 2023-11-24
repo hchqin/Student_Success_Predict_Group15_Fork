e79a4f524750ca9e85e221f997fcd9cf24a84f3d
tang yili <yltang92@student.ubc.ca>
2023-11-24 14:30:09 -0800

    fix a typo in test

--- test/test_map_func.py
+++ test/test_map_func.py
@@ -5,7 +5,7 @@ import os
 
 # Import the map_int_categories function from the src folder
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-from src.category_mapping_func import map_int_categories
+from src.map_int_category import map_int_categories
 
 # Test data and mappings
 course_mapping = {
