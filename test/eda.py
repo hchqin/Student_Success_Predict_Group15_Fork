fa6f3aba6b5a8e42c202da024f7261a3f37854d0
Bill Wan <billwan@student.ubc.ca>
2023-11-24 00:23:42 -0800

    add tests and eda.py files

---
+++ test/eda.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import numpy as np
+from pandas.testing import assert_frame_equal
+import pytest
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from src.data_analysis_EDA import calculate_correlation_df
+
+# Create a sample DataFrame for testing
+data = {
+    'A': [1, 2, 3, 4],
+    'B': [2, 4, 6, 8],
+    'C': [5, 5, 5, 5]
+    }
+df = pd.DataFrame(data)
+
+def test_correlation_calculation_with_threshold_shape():
+    threshold = 0.7
+    result_df = calculate_correlation_df(df, threshold)
+
+    # Check if the result is a DataFrame
+    assert isinstance(result_df, pd.DataFrame), "it should return a dataframe"
+    # Check if the expected columns are present
+    assert 'Variable 1' in result_df.columns, "the first variable is missing"
+    assert 'Variable 2' in result_df.columns, "the second variable is missing"
+    assert 'Correlation' in result_df.columns, "the correlation is missing"
+
+def test_correlation_calculation_accuracy():
+    result_df = calculate_correlation_df(df)
+    assert isinstance(result_df, pd.DataFrame), "The function should return a dataframe"
+    assert not result_df.empty, "The dataframe should not be empty"
+    assert 'Variable 1' in result_df.columns, "The first variable is missing"
+    assert 'Variable 2' in result_df.columns, "The second variable is missing"
+    assert 'Correlation' in result_df.columns, "The correlation is missing"
+    assert result_df['Correlation'].abs().iloc[0] == 1.0, "The correlation calculated is wrong"
+
+def test_correlation_calculation_without_threshold_shape():
+    result_df = calculate_correlation_df(df)
+
+    assert isinstance(result_df, pd.DataFrame), "it should return a dataframe"
+    assert 'Variable 1' in result_df.columns, "the first variable is missing"
+    assert 'Variable 2' in result_df.columns, "the second variable is missing"
+    assert 'Correlation' in result_df.columns, "the correlation is missing"
+
+def test_correlation_calculation_with_custom_threshold():
+    data = {
+        'X': [1, 2, 3, 4],
+        'Y': [5, 5, 5, 6]
+    }
+    df = pd.DataFrame(data)
+    threshold = 0.8
+    result_df = calculate_correlation_df(df, threshold)
+
+    assert isinstance(result_df, pd.DataFrame), "it should reutnr a dataframe"
+    assert 'Variable 1' in result_df.columns, "the first variable is missing"
+    assert 'Variable 2' in result_df.columns, "the second variable is missing"
+    assert 'Correlation' in result_df.columns, "the correlation is missing"
+    assert result_df.empty, "the result DataFrame should be empty, threshold not working"
